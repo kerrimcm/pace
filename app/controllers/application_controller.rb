38c09c6f1f446b1c57d27797dad4edd15b92268e
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-08 16:35:36 +0100

    halfway through sign up save point

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  protected
+  def configure_permitted_parameters
+    # Permit the `subscribe_newsletter` parameter along with the other
+    # sign up parameters.
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :postcode])
+  end
+  
 end
---
+++ spec/features/sign_up_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+feature 'Sign up' do
+
+  scenario 'signs up the user' do
+    visit "/"
+    click_link "Sign up"
+    fill_in "First name", with: "Jane"
+    fill_in "Postcode", with: "SW1"
+    fill_in "DOB", with: "20/04/2000"
+    fill_in "Gender", with: "Female"
+    fill_in "Running ability", with: "Basic" 
+    fill_in "Fitness Goals", with: "Intermediate"
+    fill_in "Email", with: "jdoe@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password Confirmation", with: "password"
+    click_button "Create Account"
+    expect(page).to have_content "Welcome! You have signed up successfully."
+  end
+end
