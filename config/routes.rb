126decb9ede93c8d1234d5ac6d8603a8803c5093
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-08 11:00:17 +0100

    Users profile can now be shown

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "users#index"
+  post "/users/:id", to: "users#show"
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
