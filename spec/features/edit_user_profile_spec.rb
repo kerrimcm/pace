33df0d71b724194829e82918053e7ddaa55d8a6d
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-06-08 11:47:31 +0100

    Created edit button

---
+++ spec/features/edit_user_profile_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature "Edit button" do
+  scenario "User can see an edit button" do
+    visit "/"
+    click_link "Login"
+    click_link "Sign up"
+    fill_in "Email", with: "test@test.com"
+    fill_in "Password", with: "123456"
+    fill_in "Password confirmation", with: "123456"
+    click_button "Sign up"
+    click_button "Show"
+    expect(page).to have_content "test@test.com"
+    expect(page).to have_button "Edit"
+  end
+end 
