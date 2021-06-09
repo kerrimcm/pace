ee80aff240833bea6179901cdfb1025444f9e8b9
Jessica Brand <jeshbrand@gmail.com>
2021-06-09 14:38:40 +0100

    minor test fix to home page spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -4,6 +4,6 @@ feature "Home page" do
   scenario "Users can see all other users and their info on the home page" do
     sign_up_jane
     expect(page).to have_content "Welcome! You have signed up successfully."
-    expect(page).to have_content "jdoe@email.com"
+    expect(page).to have_content "Jane"
   end
-end 
+end
