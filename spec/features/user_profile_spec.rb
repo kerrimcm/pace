8686d973ea93c4bbfddd4c7fe15b39ca9df52abe
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-09 10:25:51 +0100

    helper methods added and implemented

--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -2,14 +2,8 @@ require 'rails_helper'
 
 feature "Profile page show" do
   scenario "Users can view other users profiles" do
-    visit "/"
-    click_link "Login"
-    click_link "Sign up"
-    fill_in "Email", with: "test@test.com"
-    fill_in "Password", with: "123456"
-    fill_in "Password confirmation", with: "123456"
-    click_button "Sign up"
+    sign_up_jane
     click_button "Show"
-    expect(page).to have_content "test@test.com"
+    expect(page).to have_content "jdoe@email.com"
   end
 end 
