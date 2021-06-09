d2e61540363c18833283be53e75f7ef77f1198fc
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-06-09 11:49:01 +0100

    Created views

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,9 @@ Rails.application.routes.draw do
   post "/users/:id", to: "users#show"
   post "/users/:id/edit", to: "users#edit"
   resources :users
+  resources :conversations do
+    resources :messages
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
