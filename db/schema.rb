26deb686695f485ca0bceffaf0e23a30f90cc372
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-10 10:31:44 +0100

    small bug fix to users index

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2021_06_09_111532) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +41,7 @@ ActiveRecord::Schema.define(version: 2021_06_09_111532) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
 
   create_table "conversations", force: :cascade do |t|
     t.integer "sender_id"
