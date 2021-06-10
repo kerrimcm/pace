42be038eb587470d3fbc96ab205b2a6b0eb1fc1d
Kerri McMahon <kerrimcm25@gmail.com>
2021-06-10 11:25:04 +0100

    User can now update their info and see changes made

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
     # Permit the `subscribe_newsletter` parameter along with the other
     # sign up parameters.
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :postcode, :dob, :gender, :ability, :goals, :profile_pic])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :postcode, :dob, :gender, :ability, :goals, :profile_pic])
   end
 
 end
