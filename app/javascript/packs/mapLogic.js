2ccc7cb91a9102d84ddc5cfb841439005d127c8d
Jessica Brand <jeshbrand@gmail.com>
2021-06-11 17:43:12 +0100

    Places API now working. Locations are searchable

--- app/javascript/packs/mapLogic.js
+++ app/javascript/packs/mapLogic.js
@@ -14,10 +14,11 @@
 //   handler.bounds.extendWith(markers);
 //   handler.fitMapToBounds();
 // });
-
-function initAutocomplete() {
+console.log('I AM IN MAP LOGIC')
+window.initAutocomplete = function initAutocomplete() {
+  console.log('I AM IN initAUTOCOMPLETE!!')
   const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -33.8688, lng: 151.2195 },
+    center: { lat: 51.51284874525074, lng: -0.1573314555616207 },
     zoom: 13,
     mapTypeId: "roadmap",
   });
