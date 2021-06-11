9c8cbb4dd11a53f04f9ba0dbab0ac268adc5c054
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-11 09:25:34 +0100

    5 out of 9 failing tests now passing

--- spec/features/user_info_home_page_spec.rb
+++ spec/features/user_info_home_page_spec.rb
@@ -2,18 +2,25 @@ require 'rails_helper'
 
 feature "Home page user info spec" do
   scenario "User signs up and sees their info" do
+
+    #---------------
+
     visit "/"
     click_link "Sign up"
     fill_in "First name", with: "Jane"
     fill_in "Postcode", with: "SW1"
     fill_in "Date of birth", with: "2021-06-01"
-    select("Female", from: "Gender")
-    select("Couch", from: "Running ability")
-    select("Speed", from: "Fitness Goals")
+    select('Female', from: 'Gender')
+    select('Couch', from: 'Running ability')
+    select('Speed', from: 'Fitness Goals')
+    attach_file('Profile picture', './spec/fixtures/miss_piggy.jpg')
     fill_in "Email", with: "jdoe@email.com"
     fill_in "Password", with: "password"
     fill_in "Password confirmation", with: "password"
     click_button "Sign up"
+
+    #-------------
+
     expect(page).to have_content "Jane"
     expect(page).to have_content "SW1"
     expect(page).to have_content "Female"
--- spec/features/user_profile_spec.rb
+++ spec/features/user_profile_spec.rb
@@ -2,8 +2,26 @@ require 'rails_helper'
 
 feature "Profile page show" do
   scenario "Users can view other users profiles" do
-    sign_up_jane
-    click_button "View user"
+
+    #---------------------------------
+
+    visit "/"
+    click_link "Sign up"
+    fill_in "First name", with: "Jane"
+    fill_in "Postcode", with: "SW1"
+    fill_in "Date of birth", with: "2021-06-01"
+    select('Female', from: 'Gender')
+    select('Couch', from: 'Running ability')
+    select('Speed', from: 'Fitness Goals')
+    attach_file('Profile picture', './spec/fixtures/miss_piggy.jpg')
+    fill_in "Email", with: "jdoe@email.com"
+    fill_in "Password", with: "password"
+    fill_in "Password confirmation", with: "password"
+    click_button "Sign up"
+
+    #----------------------------------
+    
+    click_link "View profile"
     expect(page).to have_content "Jane"
     expect(page).to have_content "SW1"
     expect(page).to have_content "Female"
