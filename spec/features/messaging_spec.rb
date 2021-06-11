72f30324eef50261299a6938521a09eca3bd6bc3
Dominic Meddick <dominicmeddick@gmail.com>
2021-06-11 10:56:52 +0100

    All tests passing with helper methods working

--- spec/features/messaging_spec.rb
+++ spec/features/messaging_spec.rb
@@ -4,40 +4,9 @@ require 'rails_helper'
 feature "Users can message each other" do
   scenario "Once a user is signed up they can send and receive messages" do
 
-    #--------------------------------
-
-    visit "/"
-    click_link "Sign up"
-    fill_in "First name", with: "Jane"
-    fill_in "Postcode", with: "SW1"
-    fill_in "Date of birth", with: "2021-06-01"
-    select('Female', from: 'Gender')
-    select('Couch', from: 'Running ability')
-    select('Speed', from: 'Fitness Goals')
-    attach_file('Profile picture', './spec/fixtures/miss_piggy.jpg')
-    fill_in "Email", with: "jdoe@email.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
+    sign_up_jane
     click_link "Logout"
-
-    #-------------------------------
-
-    visit "/"
-    click_link "Sign up"
-    fill_in "First name", with: "Mark"
-    fill_in "Postcode", with: "SW2"
-    fill_in "Date of birth", with: "2000-06-01"
-    select('Male', from: 'Gender')
-    select('Marathon', from: 'Running ability')
-    select('Distance', from: 'Fitness Goals')
-    attach_file('Profile picture', './spec/fixtures/miss_piggy.jpg')
-    fill_in "Email", with: "mark@email.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
-
-    #---------------------------------
+    sign_up_mark
 
     click_button "View Jane's Profile"
     click_link "Message this user"
--- spec/features/user_can_upload_profile_picture_spec.rb
+++ spec/features/user_can_upload_profile_picture_spec.rb
@@ -3,41 +3,12 @@ require 'spec_helper'
 feature 'profile picture' do
   scenario 'at signup, user can upload an image & see it on user feed' do
 
-    #-------------------------
-    visit "/"
-    click_link "Sign up"
-    fill_in "First name", with: "Jane"
-    fill_in "Postcode", with: "SW1"
-    fill_in "Date of birth", with: "2021-06-01"
-    select('Female', from: 'Gender')
-    select('Couch', from: 'Running ability')
-    select('Speed', from: 'Fitness Goals')
-    attach_file('Profile picture', './spec/fixtures/miss_piggy.jpg')
-    fill_in "Email", with: "jdoe@email.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    click_button "Sign up"
-
-    #-------------------------
-    
+    sign_up_jane
     expect(page).to have_xpath("//img[contains(@src,'miss_piggy.jpg')]")
   end
 
   scenario 'at signup, user can upload an image & see it on profile page' do
-    visit "/"
-    click_link "Sign up"
-    fill_in "First name", with: "Jane"
-    fill_in "Postcode", with: "SW1"
-    fill_in "Date of birth", with: "2021-06-01"
-    select('Female', from: 'Gender')
-    select('Couch', from: 'Running ability')
-    select('Speed', from: 'Fitness Goals')
-    fill_in "Email", with: "jdoe@email.com"
-    fill_in "Password", with: "password"
-    fill_in "Password confirmation", with: "password"
-    attach_file('user[profile_pic]',
-                File.join(Rails.root, '/spec/fixtures/miss_piggy.jpg'), :visible => true)
-    click_button "Sign up"
+    sign_up_mark
     click_link "View profile"
     expect(page).to have_xpath("//img[contains(@src,'miss_piggy.jpg')]")
   end
