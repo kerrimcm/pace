dea33dafe092648890cf2d9af948666a13076230
Ionut Cristian Mintar <ionutcristian_mintar@yahoo.com>
2021-06-15 14:18:31 +0100

    Worked out logic in the views for displaying the unread messages next to the sender

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,6 +4,7 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
+    @unread_counts = dummy
   end
   def create  
     if Conversation.between(params[:sender_id], params[:recipient_id]).present? 
@@ -13,9 +14,14 @@ class ConversationsController < ApplicationController
     end
     redirect_to conversation_messages_path(@conversation)
   end
-  
+
   private
   def conversation_params
     params.permit(:sender_id, :recipient_id)
   end
+
+  def dummy
+    [{user_id: 4, unread_count: 6},{user_id: 2, unread_count: 0}, {user_id: 3, unread_count: 10}]
+  end 
+
 end
