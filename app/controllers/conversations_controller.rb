e31ba391e564704aae7dcb1c806651a697f0f7f5
ian-1 <iandavidwalton@mail.com>
2021-06-15 16:19:46 +0100

    feature show number of unread messages for each conversation complete

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -4,7 +4,7 @@ class ConversationsController < ApplicationController
   def index
     @users = User.all
     @conversations = Conversation.all
-    @unread_counts = dummy
+    @unread_counts = unread_array_builder
   end
   def create  
     if Conversation.between(params[:sender_id], params[:recipient_id]).present? 
@@ -20,8 +20,29 @@ class ConversationsController < ApplicationController
     params.permit(:sender_id, :recipient_id)
   end
 
-  def dummy
-    [{user_id: 4, unread_count: 6},{user_id: 2, unread_count: 0}, {user_id: 3, unread_count: 10}]
-  end 
-
+  def unread_array_builder
+    unread_messages = []
+    conversations = Conversation.all
+    conversations.each do |conversation|
+      unless current_user.nil?
+        if conversation.recipient_id == current_user.id || conversation.sender_id == current_user.id
+          if conversation.recipient_id == current_user.id
+            other_user_id = conversation.sender_id
+          else
+            other_user_id = conversation.recipient_id
+          end
+          unread_counter = 0
+          conversation.messages.each do |message|
+            unless message.user_id == current_user.id
+              if message.read.to_s == '0'
+                unread_counter += 1
+              end
+            end
+          end
+          unread_messages << { user_id: other_user_id, unread_count: unread_counter }
+        end
+      end
+    end
+    unread_messages
+  end
 end
