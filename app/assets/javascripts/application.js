4342a9b7d6168277fc8eb6b8e89a1d8926b032dc
ian-1 <iandavidwalton@mail.com>
2021-06-17 14:40:12 +0100

    removed unused css

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -28,32 +28,3 @@ if (drop2open == 1) {
 document.getElementById('menu2').style.display="none";
 document.getElementById('topmenu').classList.toggle("change");
 drop2open = 0; } }
-
-function topp() {
-  document.write(
-    '<div class="top2">'+
-      '<div class="top2b">'+
-        '<a href="/index">Home</a>'+
-        '<a href="/why">Why use a specialist?</a>'+
-        '<a href="/story">Our story</a>'+
-        '<a href="/rent">Properties</a></a>'+
-        '<a href="/contact">Contact us</a>'+
-      '</div>'+
-    '</div>'+
-    '<div id="topmenu" onclick="drop2(event)">'+
-      '<div class="bar1"></div>'+
-      '<div class="bar2"></div>'+
-      '<div class="bar3"></div>'+
-    '</div>'+
-
-    '<div id="menu2" onclick="hide2()">'+
-      '<ul>'+
-        '<li><a href="/index">Home</a></li>'+
-        '<li><a href="/why">Why use a specialist?</a></li>'+
-        '<li><a href="/story">Our story</a></li>'+
-        '<li><a href="/rent">Properties</a></li>'+
-        '<li><a href="/contact">Contact us</a></li>'+
-      '</ul>'+
-    '</div>'
-  );
- }
